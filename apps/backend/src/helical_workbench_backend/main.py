6ff8b35c7275a966e467bd046edeecc3d2f7f4dc
oriol <oriol.petit@hotmail.com>
2026-02-22 23:19:01 +0100

    [MLP] Adding CORS

--- apps/backend/src/helical_workbench_backend/main.py
+++ apps/backend/src/helical_workbench_backend/main.py
@@ -1,9 +1,16 @@
 import uvicorn
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from helical_workbench_backend.api.router import router
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app.include_router(router)
 
 
